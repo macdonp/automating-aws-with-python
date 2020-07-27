1d1e1265a897f368dd428e9dbb390cad78f456b7
Paul Macdonald <info@paulhmacdonald.com>
2020-07-27 13:42:55 +0200

    Output website host when syncing

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+"""Classes for S3 Buckets."""
+
 from pathlib import Path
-from botocore.exceptions import ClientError
 import mimetypes
 
-"""Classes for S3 Buckets."""
+from botocore.exceptions import ClientError
+
+import util
 
 
 class BucketManager:
@@ -15,12 +18,26 @@ class BucketManager:
         self.session = session
         self.s3 = self.session.resource('s3')
 
+    def get_region_name(self, bucket):
+        """Get the bucket's region name."""
+        client = self.s3.meta.client
+        bucket_location = client.get_bucket_location(Bucket=bucket.name)
+
+        return bucket_location["LocationConstraint"] or 'us-east-1'
+
+    def get_bucket_url(self, bucket):
+        """Get the website URL for this bucket."""
+        return "http://{}.{}".format(
+            bucket.name,
+            util.get_endpoint(self.get_region_name(bucket)).host
+            )
+
     def all_buckets(self):
         """Get an iterator for all buckets."""
         return self.s3.buckets.all()
 
     def all_objects(self, bucket_name):
-        """Get an iterator for all objects in a bucket."""
+        """Get an iterator for all objects in bucket."""
         return self.s3.Bucket(bucket_name).objects.all()
 
     def init_bucket(self, bucket_name):
@@ -45,60 +62,57 @@ class BucketManager:
         """Set bucket policy to be readable by everyone."""
         policy = """
         {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Sid": "PublicReadGetObject",
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": [
-                    "s3:GetObject"
-                ],
-                "Resource": [
-                    "arn:aws:s3:::%s/*"
-                ]
+          "Version":"2012-10-17",
+          "Statement":[{
+          "Sid":"PublicReadGetObject",
+          "Effect":"Allow",
+          "Principal": "*",
+              "Action":["s3:GetObject"],
+              "Resource":["arn:aws:s3:::%s/*"
+              ]
             }
-        ]
+          ]
         }
         """ % bucket.name
-
         policy = policy.strip()
+
         pol = bucket.Policy()
         pol.put(Policy=policy)
 
     def configure_website(self, bucket):
-        """Configure website."""
+        """Configure s3 website hosting for bucket."""
         bucket.Website().put(WebsiteConfiguration={
             'ErrorDocument': {
                 'Key': 'error.html'
             },
             'IndexDocument': {
                 'Suffix': 'index.html'
-                }
-                })
+            }
+        })
 
     @staticmethod
     def upload_file(bucket, path, key):
-        """Upload file."""
+        """Upload path to s3_bucket at key."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+
         return bucket.upload_file(
             path,
             key,
             ExtraArgs={
                 'ContentType': content_type
-                }
-        )
+            })
 
-        def sync(self, pathname, bucket_name):
-            """Sync for bucket."""
-            bucket = self.s3.Bucket(bucket_name)
-            root = Path(pathname).expanduser().resolve()
+    def sync(self, pathname, bucket_name):
+        """Sync contents of path to bucket."""
+        bucket = self.s3.Bucket(bucket_name)
 
-            def handle_directory(target):
-                for p in target.iterdir():
-                    if p.is_dir():
-                        handle_directory(p)
-                    if p.is_file():
-                        self.upload_file(bucket, str(p),
-                                str(p.relative_to(root)))
-            handle_directory(root)
+        root = Path(pathname).expanduser().resolve()
+
+        def handle_directory(target):
+            for p in target.iterdir():
+                if p.is_dir():
+                    handle_directory(p)
+                if p.is_file():
+                    self.upload_file(bucket, str(p), str(p.relative_to(root)))
+
+        handle_directory(root)
